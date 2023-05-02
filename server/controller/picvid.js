90b99753db1087e3f7ef5aa4968c59a5d662b7d0
wrendann <ironmuscle002@gmail.com>
2023-05-02 22:15:15 +0530

    Fixed some bugs, added more options to the search term feature, and readded the google cloud storage upload feature, and tested it.

--- server/controller/picvid.js
+++ server/controller/picvid.js
@@ -2,19 +2,34 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const { getConnection } = require('../databaseConfig');
+const uuidv4 = require('uuid').v4;
+const { Storage } = require('@google-cloud/storage');
 const mysql = require('mysql');
+const turl = require('turl');
 
 const picvidRouter = express.Router();
 
 picvidRouter.post('/pic', (req, res) => {
+  const file = req.files.file;
+
+  if (!file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+
+  if(!(file.mimetype.startsWith('image/')))
+  {
+    res.status(400).send('File is not image.');
+    return;
+  }
 
   if(!req.isAuthenticated)
     return res.status(401).json({ error: 'User not logged in.' });
   const username = req.user.username;
-  const { eventid, publicUrl } = req.body;
-  if(!eventid || !publicUrl)
+  const { eventid } = req.body;
+  if(!eventid)
   {
-    return res.status(400).json({ error: 'No eventid or public url provided.' });
+    return res.status(400).json({ error: 'No eventid provided.' });
   }
   const eventSelectSql = `SELECT * FROM Event WHERE eventid = ${mysql.escape(eventid)};`
 
@@ -33,12 +48,52 @@ picvidRouter.post('/pic', (req, res) => {
     {
       return res.status(403).json({ error: 'User not permitted to add pictures to this event.' });
     }
-    
-    getConnection().query('INSERT INTO Picture VALUES (?, ?);', [eventid, publicUrl], (result, err) => {
-      if(err)
-        return res.json(err);
-      res.status(201).send('Successfully uploaded picture.');
+
+    const storage = new Storage({
+      projectId: process.env.PROJECT_ID,
+      keyFilename: __dirname + process.env.KEY_FILENAME,
     });
+    
+    const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+    const filename = uuidv4();
+
+    const blob = bucket.file(filename);
+
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    blobStream.on('error', (err) => {
+      console.error(err);
+      return res.status(500).send('Error uploading file.');
+    });
+
+    blobStream.on('finish', async () => {
+      const config = {
+        action: 'read',
+        expires: '03-17-2025', // Optional expiration date
+      };
+      blob.getSignedUrl(config, (err, url) => {
+        if (err) {
+          return res.status(500).send('Error getting file url');
+        }
+        turl.shorten(url).then((publicUrl) => {
+          getConnection().query('INSERT INTO Picture VALUES (?, ?, ?);', [eventid, publicUrl, filename], (err, result) => {
+            if(err)
+              return res.json(err);
+            return res.status(201).json({eventid, publicUrl, filename});
+          });
+        }).catch((err) => {
+          return res.status(500).json(err);
+        });
+      });
+    });
+
+    blobStream.end(file.data);
   });
 });
 
@@ -66,11 +121,33 @@ picvidRouter.delete('/pic', (req, res) => {
     {
       return res.status(403).json({ error: 'User not permitted to delete pictures from this event.' });
     }
-   
-    getConnection().query('DELETE FROM Picture WHERE eventid = ? AND url = ?;', [eventid, publicUrl], (result, err) => {
+    const storage = new Storage({
+      projectId: process.env.PROJECT_ID,
+      keyFilename: __dirname + process.env.KEY_FILENAME,
+    });
+    
+    const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+    getConnection().query('SELECT * FROM Picture WHERE eventid = ? AND url = ?;', [eventid, publicUrl], (err, result) => {
       if(err)
         return res.json(err);
-      res.status(203).send('Successfully deleted picture.');
+
+      if(result.length == 0)
+        return res.status(404).json({ error: 'Picture not found.' });
+
+      const filename = result[0].filename;
+
+      const file = bucket.file(filename);
+
+      file.delete().then(() => {
+        getConnection().query('DELETE FROM Picture WHERE eventid = ? AND url = ?;', [eventid, publicUrl], (err, result) => {
+          if(err)
+            return res.json(err);
+          return res.status(203).send('Successfully deleted picture.');
+        });
+      }).catch((err) => {
+        return res.status(400).json(err);
+      });
     });
   });
 });
@@ -103,7 +180,7 @@ picvidRouter.post('/vid', (req, res) => {
       return res.status(403).json({ error: 'User not permitted to add videos to this event.' });
     }
     
-    getConnection().query('INSERT INTO Video VALUES (?, ?);', [eventid, publicUrl], (result, err) => {
+    getConnection().query('INSERT INTO Video VALUES (?, ?);', [eventid, publicUrl], (err, result) => {
       if(err)
         return res.json(err);
       res.status(201).send('Successfully uploaded video.');
